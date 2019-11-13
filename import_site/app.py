a9b8c77d2ecf45816d2f29f51da9122aeb49ced8
roy4801 <a82611141@gmail.com>
2019-11-13 19:26:53 +0800

    Can add users and delete them and add forms

--- import_site/app.py
+++ import_site/app.py
@@ -15,10 +15,35 @@ def index():
 
 @app.route('/test')
 def test():
-	utils.create_new_user('team17', '406262515')
-	utils.add_user_to_contest(4, 2)
+	s = None
+	with open('list.csv', 'r') as f:
+		s = f.readlines()
+	li = []
+	for i in s:
+		row = i.split(',')
+		li.append((row[0], row[1].rstrip()))
+
+	for user, passwd in li:
+		utils.create_new_user('team'+user, passwd)
+	# utils.create_new_user('team17', '406262515')
+	# utils.add_user_to_contest(4, 2) # uid cid
 	# utils.delete_all_imported_users()
-	return 'Go to see the console'
+	return 'Success'
+
+@app.route('/add')
+def add():
+	from sqlalchemy import text
+	sql = text('INSERT INTO contestteam (cid, teamid) VALUES (:cid, :tid)')
+	for i in range(6, 43):
+		print(i)
+		db.session.execute(sql, {'cid': 3, 'tid': i})
+	db.session.commit()
+	return 'Success'
+
+@app.route('/delall')
+def del_all():
+	utils.delete_all_imported_users()
+	return 'Success'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
